f9f983fd2bc62caf7d617a8d29fa5c8bb19c6499
ponentesincausa <ponentesincausa@gmail.com>
2018-01-06 15:56:12 -0600

    added User/Project/Task functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :projects do
-    resources :tasks do
-      resources :comments
+  # resources :users do
+    resources :projects do
+      resources :tasks do
+        resources :comments
+      end
     end
-  end
+  # end
   devise_for :users
   get 'home/index'
-  root 'home#index'
+  root 'projects#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180104053939) do
+ActiveRecord::Schema.define(version: 20180104061403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20180104053939) do
     t.bigint "project_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "deadline"
     t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
